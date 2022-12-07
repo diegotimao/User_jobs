18d12de28254a164a15378a56412c8750814b947
diegotimao <diegotimao1104@gmail.com>
2022-12-06 20:23:01 -0500

    feat: Buscando dados filtrado pelo type_jobs

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -22,18 +22,4 @@ def get_unique_job_types(path: str) -> List[str]:
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
+    return [item for item in jobs if item["job_type"] == job_type]
