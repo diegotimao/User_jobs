4d34af08bea04d51814dd3620d515978b13aa452
diegotimao <diegotimao1104@gmail.com>
2022-12-06 21:21:16 -0500

    feat: Implementando filtro para buscar uma industry

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -14,18 +14,4 @@ def get_unique_industries(path: str) -> List[str]:
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    return [item for item in jobs if item["industry"] == industry]
