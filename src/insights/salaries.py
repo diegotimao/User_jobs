9d837a12acc71bb45f3ca894db455db3dfc95832
diegotimao <diegotimao1104@gmail.com>
2022-12-11 14:30:52 -0500

    feat: Lança um ValueError se o valor de min_salary for maior que o valor de max_salary

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -8,8 +8,7 @@ def get_max_salary(path: str) -> int:
     return max(
         [
             int(salary["max_salary"])
-            for salary
-            in file_csv
+            for salary in file_csv
             if (salary["max_salary"]).isdigit()
         ]
     )
@@ -21,37 +20,25 @@ def get_min_salary(path: str) -> int:
     return min(
         [
             int(salary["min_salary"])
-            for salary
-            in file_csv
+            for salary in file_csv
             if (salary["min_salary"]).isdigit()
         ]
     )
 
 
+def veryfy_keys(jobs_keys):
+    min_salaryes = jobs_keys["min_salary"]
+    max_salaryes = jobs_keys["max_salary"]
+
+    if max_salaryes < min_salaryes:
+        raise ValueError
+
+
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
+    try:
+        veryfy_keys(job)
+    except ValueError:
+        print("Erro")
 
 
 def filter_by_salary_range(
