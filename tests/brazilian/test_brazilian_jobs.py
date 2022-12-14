72e17923c1b21e321e555910aed10eaacb9db797
diegotimao <diegotimao1104@gmail.com>
2022-12-14 14:55:04 -0500

    test: Implemtando teste para verificar se a função retorna as chaves do dicionario em inglês

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,10 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    list_brazilian = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+
+    for bras in list_brazilian:
+        response = "title" in bras or "salary" in bras or "type" in bras
+
+        assert response is True
