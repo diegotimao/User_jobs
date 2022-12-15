3a02f1634f298c49c220b4eac15b5f73eaf9cc48
diegotimao <diegotimao1104@gmail.com>
2022-12-14 21:16:41 -0500

    reafactor: Refatorando teste que ordena pelo min_salary

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,43 +1,19 @@
 from src.pre_built.sorting import sort_by
 
-mock_jobs = [
-    {
-        "date_posted": "2018-03-03",
-        "max_salary": "1650",
-        "min_salary": "1600",
-    },
-    {
-        "date_posted": "2018-03-01",
-        "max_salary": "2500",
-        "min_salary": "1000",
-    },
-    {
-        "date_posted": "2018-03-02",
-        "max_salary": "1900",
-        "min_salary": "1500",
-    },
+mock_jobs_min = [
+    {"date_posted": "2018-03-03", "max_salary": "3950", "min_salary": "1600"},
+    {"date_posted": "2018-03-01", "max_salary": "2100", "min_salary": "1500"},
+    {"date_posted": "2018-03-02", "max_salary": "1900", "min_salary": "1200"},
 ]
 
-
-mock_jobs_sorted_min_salary = [
-    {
-        "date_posted": "2018-03-01",
-        "max_salary": "2500",
-        "min_salary": "1000",
-    },
-    {
-        "date_posted": "2018-03-02",
-        "max_salary": "1900",
-        "min_salary": "1500",
-    },
-    {
-        "date_posted": "2018-03-03",
-        "max_salary": "1650",
-        "min_salary": "1600",
-    },
+mock_jobs_min_sorted = [
+    {"date_posted": "2018-03-02", "max_salary": "1900", "min_salary": "1200"},
+    {"date_posted": "2018-03-01", "max_salary": "2100", "min_salary": "1500"},
+    {"date_posted": "2018-03-03", "max_salary": "3950", "min_salary": "1600"},
 ]
 
 
 def test_sort_by_criteria():
-    sort_by(mock_jobs, "min_salary")
-    assert mock_jobs == mock_jobs_sorted_min_salary
+
+    sort_by(mock_jobs_min, "min_salary")
+    assert mock_jobs_min == mock_jobs_min_sorted
